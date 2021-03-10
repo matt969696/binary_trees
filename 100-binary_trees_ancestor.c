bd3c930d2bd2f4a9ffd5a1c9139bcc146c983d5d
aureliewouy <aureliecedia@yahoo.com>
2021-03-10 12:46:08 +0000

    fix betty

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -24,7 +24,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
  * Return: lowest common ancestor
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
 {
 	size_t df, ds;
 	int i;
